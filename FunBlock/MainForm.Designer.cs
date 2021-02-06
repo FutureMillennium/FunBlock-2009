73bfc2b14328de8e1749d7b5fd95839d96ebb29b
Zdeněk Gromnica <futuremillennium@gmail.com>
2021-02-06 19:52:43 +0100

    version from 2009-11-05

---
+++ FunBlock/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FunBlock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtNewHosts = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sttStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtNewHosts
+            // 
+            this.txtNewHosts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtNewHosts.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewHosts.Location = new System.Drawing.Point(0, 0);
+            this.txtNewHosts.Multiline = true;
+            this.txtNewHosts.Name = "txtNewHosts";
+            this.txtNewHosts.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtNewHosts.Size = new System.Drawing.Size(362, 246);
+            this.txtNewHosts.TabIndex = 0;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "FunBlock";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(106, 26);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.quitToolStripMenuItem.Text = "&Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sttStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 246);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(362, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sttStatus
+            // 
+            this.sttStatus.Name = "sttStatus";
+            this.sttStatus.Size = new System.Drawing.Size(109, 17);
+            this.sttStatus.Text = "toolStripStatusLabel1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 268);
+            this.Controls.Add(this.txtNewHosts);
+            this.Controls.Add(this.statusStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "FunBlock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtNewHosts;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sttStatus;
+    }
+}
+
---
+++ FunBlock/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FunBlock
+{
+    public partial class MainForm : Form
+    {
+        bool blChanged;
+        string strOldHosts;
+
+        // Function: Read string from a file
+        private string ReadFile(string file)
+        {
+            StreamReader reader = new StreamReader(file, Encoding.Default);
+            string data = reader.ReadToEnd();
+            reader.Close();
+
+            return data;
+        }
+
+        // Function: Save string to a file
+        private void SaveFile(string file, string data)
+        {
+            StreamWriter writer = new StreamWriter(file);
+            writer.Write(data);
+            writer.Close();
+        }
+
+        void ChangeStatus()
+        {
+            if (blChanged)
+            {
+                sttStatus.Text = "Active";
+            }
+            else
+            {
+                sttStatus.Text = "Not active";
+            }
+
+            notifyIcon1.Text = this.Text + " - " + sttStatus.Text;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blChanged = FunBlock.Properties.Settings.Default.Changed;
+            txtNewHosts.Text = FunBlock.Properties.Settings.Default.NewHosts;
+
+            if (blChanged == false)
+                strOldHosts = ReadFile(@"C:\Windows\System32\drivers\etc\hosts");
+            else
+            {
+                this.Hide();
+                strOldHosts = FunBlock.Properties.Settings.Default.OldHosts;
+            }
+
+            notifyIcon1.Icon = this.Icon;
+
+            ChangeStatus();
+            timer1_Tick(null, null);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dtmNow = DateTime.Now;
+
+            if (dtmNow.Hour >= 20 && dtmNow.Hour < 23 && blChanged == true)
+            {
+                SaveFile(@"C:\Windows\System32\drivers\etc\hosts", strOldHosts);
+                blChanged = false;
+                ChangeStatus();
+            }
+            else if ((dtmNow.Hour < 20 || dtmNow.Hour >= 23) && blChanged == false)
+            {
+                SaveFile(@"C:\Windows\System32\drivers\etc\hosts", strOldHosts + txtNewHosts.Text);
+                blChanged = true;
+                ChangeStatus();
+            }
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+            else
+            {
+                FunBlock.Properties.Settings.Default.Changed = blChanged;
+                FunBlock.Properties.Settings.Default.OldHosts = strOldHosts;
+                FunBlock.Properties.Settings.Default.NewHosts = txtNewHosts.Text;
+                FunBlock.Properties.Settings.Default.Save();
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = !this.Visible;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+    }
+}
